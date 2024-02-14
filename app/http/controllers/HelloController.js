9f29137f0662daa9b62b8560807c835d61749600
WinNKyawWilly <winnaingkyaw_win@cmu.ac.th>
2024-02-14 18:02:37 +0700

    Added Bootstrap in dashboard

--- app/http/controllers/HelloController.js
+++ app/http/controllers/HelloController.js
@@ -3,6 +3,9 @@ class HelloController {
   index(req, res, next) {
     res.render("index", { title: "Express" });
   }
+  dashboard(req, res, next) {
+    res.render("admin/dashboard", { title: "Express" });
+  }
 }
 
 module.exports = new HelloController();
--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', HelloController.index);
 
+
+/* GET admin dashboard. */
+router.get('/admin/dashboard', HelloController.dashboard);
 module.exports = router;
